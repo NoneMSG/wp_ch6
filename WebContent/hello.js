88ba50981af8193ae4808bfb9aaf39cc6c3c7266
bit-user <princeps90@gmail.com>
2017-06-28 15:12:28 +0900

    ;

---
+++ WebContent/hello.js
@@ -0,0 +1,29 @@
+var i = 10;
+var test= ()=>{
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" ); 
+	document.write( "<h2>Hello World</h2>" );
+}
+
+//
+//setTimeout( ()=>{
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" ); 
+//	document.write( "<h2>Hello World</h2>" );
+//},2000);
+ 
+setTimeout(test,2000);
